c66c8caf5675cbbe2162d05e209781e632f12a25
insung <inh3011@gmail.com>
2021-07-07 18:12:35 +0900

    File Upload part One

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 import { ApolloServer } from "apollo-server";
-import schema, { typeDefs, resolvers } from "./schema";
+import { typeDefs, resolvers } from "./schema";
 import { getUser, protectResolver } from "./users/users.utils";
 
 const server = new ApolloServer({
