21430405bd3413a77ca8fbecc4b368a57c299be0
insung <inh3011@gmail.com>
2021-07-07 18:12:06 +0900

    File Upload part One

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -2,44 +2,45 @@ import bcrypt from "bcrypt";
 import client from "../../client";
 import { protectResolver } from "../users.utils";
 
+const resolverFn = async (
+  _,
+  { firstName, lastName, username, email, password: newPassword, bio },
+  { loggedInUser }
+) => {
+  console.log(loggedInUser);
+  let uglyPassword = null;
+  if (newPassword) {
+    uglyPassword = await bcrypt.hash(newPassword, 10);
+  }
+  const updatedUser = await client.user.update({
+    where: {
+      id: loggedInUser.id,
+    },
+    data: {
+      firstName,
+      lastName,
+      username,
+      email,
+      bio,
+      // password: uglyPassword,
+      // If uglyPassword is 'true', password is uglyPassword
+      ...(uglyPassword && { password: uglyPassword }),
+    },
+  });
+  if (updatedUser.id) {
+    return {
+      ok: true,
+    };
+  } else {
+    return {
+      ok: false,
+      error: "Could not update profile.",
+    };
+  }
+};
+
 export default {
   Mutation: {
-    editProfile: protectResolver(
-      async (
-        _,
-        { firstName, lastName, username, email, password: newPassword },
-        { loggedInUser, protectResolver }
-      ) => {
-        console.log(loggedInUser);
-        let uglyPassword = null;
-        if (newPassword) {
-          uglyPassword = await bcrypt.hash(newPassword, 10);
-        }
-        const updatedUser = await client.user.update({
-          where: {
-            id: loggedInUser.id,
-          },
-          data: {
-            firstName,
-            lastName,
-            username,
-            email,
-            // password: uglyPassword,
-            // If uglyPassword is 'true', password is uglyPassword
-            ...(uglyPassword && { password: uglyPassword }),
-          },
-        });
-        if (updatedUser.id) {
-          return {
-            ok: true,
-          };
-        } else {
-          return {
-            ok: false,
-            error: "Could not update profile.",
-          };
-        }
-      }
-    ),
+    editProfile: protectResolver(resolverFn),
   },
 };
--- users/users.utils.js
+++ users/users.utils.js
@@ -19,12 +19,14 @@ export const getUser = async (token) => {
   }
 };
 
-export const protectResolver = (ourResolver) => (root, args, context, info) => {
-  if (!context.loggedInUser) {
-    return {
-      ok: false,
-      error: "Please log in to perform this action.",
-    };
-  }
-  return ourResolver(root, args, context, info);
-};
+export function protectResolver(ourResolver) {
+  return function (root, args, context, info) {
+    if (!context.loggedInUser) {
+      return {
+        ok: false,
+        error: "Please log in to perform this action.",
+      };
+    }
+    return ourResolver(root, args, context, info);
+  };
+}
