a9d2d73e0e6d41c0c02705d20763c15a8b616a21
insung <inh3011@gmail.com>
2021-07-12 23:40:30 +0900

    fucking error

---
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -0,0 +1,41 @@
+import client from "../../client";
+import { protectResolver } from "../../users/users.utils";
+
+export default {
+  Mutation: {
+    uploadPhoto: protectResolver(
+      async (_, { file, caption }, { loggedInUser }) => {
+        let hashtagObj = [];
+        if (caption) {
+          // parse caption
+          // get or create Hashtags
+
+          const hashtags = caption.match(/#[\w]+/g);
+          hashtagObj = hashtags.map((hashtag) => ({
+            where: { hashtag },
+            create: { hashtag },
+          }));
+        }
+
+        return client.photo.create({
+          data: {
+            file,
+            caption,
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
+            },
+            ...(hashtagObj.length > 0 && {
+              hashtags: {
+                connectOrCreate: hashtagObj,
+              },
+            }),
+          },
+        });
+        // save the photo with the parsed hashtags
+        // add the photo to the hashtags
+      }
+    ),
+  },
+};
